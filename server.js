2b73f6e0f7757056d7d844184bff33618b2bd84e
Alex Olson <alex.olson@utoronto.ca>
2024-09-09 19:39:13 -0400

    bug fix

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ const getEconomicRegionData = (regionName, regionURL) => {
 };
 
 // Start the server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
